183ec435c631853592fcc471cf1f112717900cfa
Arthur Sudre <arthursudre@gmail.com>
2018-04-09 15:22:36 +0200

    refactor(firebase): anonymize firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,11 @@
 export {
   config:
   {
-    apiKey: 'AIzaSyDvgQaGMBum1jTwLYzU4qwjZUNJaRgR9mA',
-    authDomain: 'mythic-hulling-163311.firebaseapp.com',
-    databaseURL: 'https://mythic-hulling-163311.firebaseio.com',
-    projectId: 'mythic-hulling-163311',
-    storageBucket: 'mythic-hulling-163311.appspot.com',
-    messagingSenderId: '891582917788'
+    apiKey: 'XXXXXXX',
+    authDomain: 'XXXXXXX',
+    databaseURL: 'XXXXXXX',
+    projectId: 'XXXXXXX',
+    storageBucket: 'XXXXXXX',
+    messagingSenderId: 'XXXXXXX',
   }
 };
